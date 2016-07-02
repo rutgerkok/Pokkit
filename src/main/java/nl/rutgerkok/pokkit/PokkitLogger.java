dc4a8a40e1a4c02161555d084b8a275869c0bf21
rutgerkok <rutger.kok@live.com>
2016-07-02 10:33:00 +0200

    Fix Bukkit plugin loggers
    Now PokkitLogger is handler-based, which means that it can now be used
    as a parent for other loggers. This makes Bukkit's PluginLogger work
    again.

--- src/main/java/nl/rutgerkok/pokkit/PokkitLogger.java
+++ src/main/java/nl/rutgerkok/pokkit/PokkitLogger.java
@@ -1,5 +1,7 @@
 package nl.rutgerkok.pokkit;
 
+import java.util.Objects;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -13,44 +15,64 @@ import cn.nukkit.utils.LogLevel;
  */
 public class PokkitLogger extends Logger {
 
-    private final PluginLogger nukkit;
+    /**
+     * The forwarding handler.
+     */
+    private static class PokkitHandler extends Handler {
+
+        private final PluginLogger nukkit;
+
+        public PokkitHandler(PluginLogger nukkit) {
+            this.nukkit = Objects.requireNonNull(nukkit, "nukkit");
+        }
+
+        @Override
+        public void close() throws SecurityException {
+            // Empty!
+        }
+
+        @Override
+        public void flush() {
+            // Empty!
+        }
+
+        @Override
+        public void publish(LogRecord record) {
+            String message = record.getMessage();
+            LogLevel level = toNukkitLevel(record.getLevel());
+            if (level == null) {
+                return;
+            }
+
+            Throwable thrown = record.getThrown();
+            if (thrown == null) {
+                nukkit.log(level, message);
+            } else {
+                nukkit.log(level, message, thrown);
+            }
+        }
+
+        private LogLevel toNukkitLevel(Level level) {
+            if (level == Level.SEVERE) {
+                return LogLevel.ERROR;
+            }
+            if (level == Level.WARNING) {
+                return LogLevel.WARNING;
+            }
+            if (level == Level.INFO) {
+                return LogLevel.INFO;
+            }
+            if (level == Level.CONFIG || level == Level.FINE) {
+                return LogLevel.DEBUG;
+            }
+            return null;
+        }
+    }
 
     protected PokkitLogger(PluginLogger nukkit) {
         super(Pokkit.NAME, null);
         this.setLevel(Level.FINE);
-        this.nukkit = nukkit;
-    }
-
-    @Override
-    public void log(LogRecord record) {
-        String message = record.getMessage();
-        LogLevel level = toNukkitLevel(record.getLevel());
-        if (level == null) {
-            return;
-        }
-
-        Throwable thrown = record.getThrown();
-        if (thrown == null) {
-            nukkit.log(level, message);
-        } else {
-            nukkit.log(level, message, thrown);
-        }
-    }
-
-    private LogLevel toNukkitLevel(Level level) {
-        if (level == Level.SEVERE) {
-            return LogLevel.ERROR;
-        }
-        if (level == Level.WARNING) {
-            return LogLevel.WARNING;
-        }
-        if (level == Level.INFO) {
-            return LogLevel.INFO;
-        }
-        if (level == Level.CONFIG || level == Level.FINE) {
-            return LogLevel.DEBUG;
-        }
-        return null;
+        this.addHandler(new PokkitHandler(nukkit));
     }
 
 }
