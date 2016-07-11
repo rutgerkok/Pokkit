0b6e3cf73543d68a211c336ad184e67c074a3463
rutgerkok <rutger.kok@live.com>
2016-07-11 09:58:10 +0200

    Support null ItemStacks

--- src/main/java/nl/rutgerkok/pokkit/material/PokkitItemStack.java
+++ src/main/java/nl/rutgerkok/pokkit/material/PokkitItemStack.java
@@ -11,6 +11,9 @@ import cn.nukkit.item.Item;
 public final class PokkitItemStack {
 
     public static ItemStack toBukkitCopy(cn.nukkit.item.Item nukkit) {
+        if (nukkit == null) {
+            return null;
+        }
         int combinedBukkitId = PokkitMaterialData.nukkitToBukkit(nukkit);
         ItemStack bukkit = new ItemStack(PokkitMaterialData.getMaterial(combinedBukkitId),
                 nukkit.getCount(), (short) PokkitMaterialData.getBlockData(combinedBukkitId));
@@ -19,6 +22,9 @@ public final class PokkitItemStack {
     }
 
     public static final cn.nukkit.item.Item toNukkitCopy(ItemStack bukkit) {
+        if (bukkit == null) {
+            return null;
+        }
         int combinedNukkitId = PokkitMaterialData.bukkitToNukkit(bukkit.getType(), bukkit.getDurability());
         cn.nukkit.item.Item nukkit = Item.get(PokkitMaterialData.getNukkitBlockId(combinedNukkitId),
                 PokkitMaterialData.getBlockData(combinedNukkitId), bukkit.getAmount());
