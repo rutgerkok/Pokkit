0f8166ba6406063747e310da56a324ea2444ca1f
rutgerkok <rutger.kok@live.com>
2016-07-30 10:33:46 +0200

    Add environment check on startup
    Nukkit ships with Google Collections, but Bukkit requires Google Guava.
    A message has been added to prompt users to uninstall Google Collections
    first.

--- src/main/java/nl/rutgerkok/pokkit/Pokkit.java
+++ src/main/java/nl/rutgerkok/pokkit/Pokkit.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
-
-import cn.nukkit.plugin.PluginBase;
 import nl.rutgerkok.pokkit.pluginservice.MainScoreboardService;
 import nl.rutgerkok.pokkit.pluginservice.PermissionsYml;
 import nl.rutgerkok.pokkit.pluginservice.PlayerBlockEvents;
@@ -17,6 +14,12 @@ import nl.rutgerkok.pokkit.pluginservice.PlayerConnectEvents;
 import nl.rutgerkok.pokkit.pluginservice.PlayerInteractEvents;
 import nl.rutgerkok.pokkit.pluginservice.PluginService;
 import nl.rutgerkok.pokkit.pluginservice.PokkitService;
+
+import org.bukkit.Bukkit;
+
+import com.google.common.collect.Maps;
+
+import cn.nukkit.plugin.PluginBase;
 
 /**
  * Startup class.
@@ -41,6 +44,8 @@ public final class Pokkit extends PluginBase {
         throw new UnsupportedOperationException("This method is not supported yet by " + NAME + " " + VERSION);
     }
 
+    private boolean errored = false;
+
     private final List<PokkitService> services = Arrays.asList(
             new MainScoreboardService(),
             new PermissionsYml(),
@@ -50,13 +55,23 @@ public final class Pokkit extends PluginBase {
             new PlayerChatEvents(),
             new PlayerInteractEvents());
 
+    private void error(String error) {
+        Bukkit.getServer().getLogger().severe(error);
+    }
+
     @Override
     public void onDisable() {
+        if (errored) {
+            return;
+        }
         services.forEach(service -> service.onDisable(this));
     }
 
     @Override
     public void onEnable() {
+        if (errored) {
+            return;
+        }
         services.forEach(service -> service.onEnable(this));
     }
 
@@ -73,6 +88,32 @@ public final class Pokkit extends PluginBase {
         PokkitServer server = new PokkitServer(this.getServer(), logger, pluginFolder);
         Bukkit.setServer(server);
 
-        services.forEach(service -> service.onLoad(this));
+        try {
+            Maps.newHashMap(); // Test whether Google Guava is present
+
+            // Load the services
+            services.forEach(service -> service.onLoad(this));
+        } catch (NoSuchMethodError e) {
+            if (!e.getMessage().contains("com.google.common.collect.")) {
+                // Some unrelated error
+                throw e;
+            }
+            // Google Collections (provided by Nukkit) is loaded instead of
+            // Guava. This is problematic, as we cannot load any Bukkit plugins
+            // this way.
+            error("Thanks for installing " + NAME + "! To complete the installation,");
+            error("open up the Nukkit JAR file with a ZIP editor (like 7-zip) and delete");
+            error("the com/google/common folder. Do NOT delete any other folder.");
+            error("Unfortunately, this is a hard requirement for " + NAME + " to load at");
+            error("all. Nukkit should still work fine after deleting the directory as long");
+            error("as " + NAME + " is installed: " + NAME + " ships with a Bukkit-compatible");
+            error("replacement of the contents of that folder.");
+            error("If you ever decide to uninstall " + NAME + ", you'll need an untouched");
+            error("copy of the Nukkit JAR file again, so please keep a backup of the Nukkit");
+            error("JAR file before you modify it.");
+            setEnabled(false);
+            errored = true;
+        }
+
     }
 }
