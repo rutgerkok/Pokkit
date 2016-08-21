13e4d3056eb7dc1859858fb7617bc7645b0a7ca1
Rutger Kok <rutger.kok@live.com>
2016-08-21 20:23:11 +0200

    Make Pokkit and Spigot version auto-update

--- src/main/java/nl/rutgerkok/pokkit/Pokkit.java
+++ src/main/java/nl/rutgerkok/pokkit/Pokkit.java
@@ -1,7 +1,11 @@
 package nl.rutgerkok.pokkit;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -26,9 +30,9 @@ import cn.nukkit.plugin.PluginBase;
  */
 public final class Pokkit {
 
-    public static final String NAME = "Pokkit";
-    public static final String VERSION = "0.0.1-SNAPSHOT";
-    public static final String BUKKIT_VERSION = "1.10.2-R0.1-SNAPSHOT";
+    public static String NAME = "Pokkit";
+    static String VERSION = "?";
+    static String BUKKIT_VERSION = "?";
 
     /**
      * Use {@code throw Pokkit.unsupported()} to indicate that a Bukkit API
@@ -52,6 +56,22 @@ public final class Pokkit {
         // Created using reflection
     }
 
+    private void loadVersionNumbers(Logger logger) {
+        Properties properties = new Properties();
+
+        // Retrieving our version is easy
+        VERSION = Pokkit.class.getPackage().getImplementationVersion();
+
+        // Retrieving Spigot version is more difficult
+        try (InputStream stream = Pokkit.class
+                .getResourceAsStream("/META-INF/maven/org.spigotmc/spigot-api/pom.properties")) {
+            properties.load(stream);
+            BUKKIT_VERSION = properties.getProperty("version");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error loading Spigot version", e);
+        }
+    }
+
     public void onDisable(PluginBase plugin) {
         services.forEach(service -> service.onDisable(plugin));
     }
@@ -61,6 +81,7 @@ public final class Pokkit {
     }
 
     public void onLoad(PluginBase plugin) {
+
         File pluginFolder = new File(plugin.getDataFolder(), "bukkitPlugins");
         if (!pluginFolder.exists()) {
             pluginFolder.mkdirs();
@@ -69,7 +90,7 @@ public final class Pokkit {
         Logger logger = new PokkitLogger(plugin.getLogger());
         LogManager.getLogManager().addLogger(logger);
 
-        System.out.println("HELLO, classloader: " + this.getClass().getClassLoader().getClass());
+        loadVersionNumbers(logger);
 
         PokkitServer server = new PokkitServer(plugin.getServer(), logger, pluginFolder);
         Bukkit.setServer(server);
