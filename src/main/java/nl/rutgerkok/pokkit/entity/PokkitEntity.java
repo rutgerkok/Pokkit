fdc4ceaf00cf14849b0e028ece1ad701346899a4
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-10-06 18:31:32 -0300

    Add PokkitEntity

---
+++ src/main/java/nl/rutgerkok/pokkit/entity/PokkitEntity.java
@@ -0,0 +1,462 @@
+package nl.rutgerkok.pokkit.entity;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.bukkit.EntityEffect;
+import org.bukkit.Location;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
+
+public class PokkitEntity implements Entity {
+	public static PokkitEntity toBukkit(cn.nukkit.entity.Entity nukkit) {
+		if (nukkit == null) {
+			return null;
+		}
+		
+		return new PokkitEntity(nukkit);
+	}
+
+	public static cn.nukkit.entity.Entity toNukkit(Entity entity) {
+		if (entity == null) {
+			return null;
+		}
+		return ((PokkitEntity) entity).nukkit;
+	}
+
+	public PokkitEntity(cn.nukkit.entity.Entity nukkitEntity) {
+		this.nukkit = nukkitEntity;
+	}
+	
+	private final cn.nukkit.entity.Entity nukkit;
+	
+	@Override
+	public void setMetadata(String metadataKey, MetadataValue newMetadataValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<MetadataValue> getMetadata(String metadataKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasMetadata(String metadataKey) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeMetadata(String metadataKey, Plugin owningPlugin) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMessage(String[] messages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPermissionSet(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isPermissionSet(Permission perm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasPermission(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasPermission(Permission perm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin plugin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin plugin, int ticks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeAttachment(PermissionAttachment attachment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recalculatePermissions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOp() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setOp(boolean value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Location getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Location getLocation(Location loc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setVelocity(Vector velocity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vector getVelocity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOnGround() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public World getWorld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean teleport(Location location) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean teleport(Location location, TeleportCause cause) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean teleport(Entity destination) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean teleport(Entity destination, TeleportCause cause) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Entity> getNearbyEntities(double x, double y, double z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getEntityId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getFireTicks() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxFireTicks() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setFireTicks(int ticks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isDead() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Server getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entity getPassenger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setPassenger(Entity passenger) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eject() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public float getFallDistance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setFallDistance(float distance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLastDamageCause(EntityDamageEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EntityDamageEvent getLastDamageCause() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID getUniqueId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTicksLived() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTicksLived(int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playEffect(EntityEffect type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EntityType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isInsideVehicle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean leaveVehicle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Entity getVehicle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCustomName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getCustomName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCustomNameVisible(boolean flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isCustomNameVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setGlowing(boolean flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isGlowing() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setInvulnerable(boolean flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isInvulnerable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSilent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSilent(boolean flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasGravity() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setGravity(boolean gravity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getPortalCooldown() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setPortalCooldown(int cooldown) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Spigot spigot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
