b8def11766ac7cde8ddbb09c5a4d90955289bcf2
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-10-06 22:14:35 -0300

    Implement PokkitWorld#spawnParticle(...)
    No offsetX, offsetY, offsetZ, count, data or extra support yet, but this
    should fix some plugins that requires particles (as long they use the
    default API instead of using reflection/packets)
    
    Fixes TrailGUI: http://i.imgur.com/9xMhKbo.png

--- src/main/java/nl/rutgerkok/pokkit/world/PokkitWorld.java
+++ src/main/java/nl/rutgerkok/pokkit/world/PokkitWorld.java
@@ -53,6 +53,7 @@ import org.bukkit.util.Vector;
 
 import cn.nukkit.level.Level;
 import cn.nukkit.level.format.generic.BaseFullChunk;
+import cn.nukkit.level.particle.GenericParticle;
 import cn.nukkit.math.Vector3;
 
 public final class PokkitWorld implements World {
@@ -764,82 +765,203 @@ public final class PokkitWorld implements World {
 
 	@Override
 	public void spawnParticle(Particle particle, double x, double y, double z, int count) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, x, y, z, count);
 	}
 
 	@Override
 	public void spawnParticle(Particle particle, double x, double y, double z, int count, double offsetX,
 			double offsetY, double offsetZ) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, x, y, z, count, offsetX, offsetY, offsetZ);
 	}
 
 	@Override
 	public void spawnParticle(Particle particle, double x, double y, double z, int count, double offsetX,
 			double offsetY, double offsetZ, double extra) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, x, y, z, count, offsetX, offsetY, offsetZ, extra);
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, double x, double y, double z, int count, double offsetX,
 			double offsetY, double offsetZ, double extra, T data) {
-		throw Pokkit.unsupported();
-
+		int id = 0;
+		
+		switch(particle) {
+		case BARRIER:
+			break;
+		case BLOCK_CRACK:
+			break;
+		case BLOCK_DUST:
+			break;
+		case CLOUD:
+			id = cn.nukkit.level.particle.Particle.TYPE_SMOKE;
+			break;
+		case CRIT:
+			id = cn.nukkit.level.particle.Particle.TYPE_CRITICAL;
+			break;
+		case CRIT_MAGIC:
+			break;
+		case DAMAGE_INDICATOR:
+			break;
+		case DRAGON_BREATH:
+			break;
+		case DRIP_LAVA:
+			id = cn.nukkit.level.particle.Particle.TYPE_DRIP_LAVA;
+			break;
+		case DRIP_WATER:
+			id = cn.nukkit.level.particle.Particle.TYPE_DRIP_WATER;
+			break;
+		case ENCHANTMENT_TABLE:
+			id = cn.nukkit.level.particle.Particle.TYPE_ENCHANTMENT_TABLE;
+			break;
+		case END_ROD:
+			break;
+		case EXPLOSION_HUGE:
+			id = cn.nukkit.level.particle.Particle.TYPE_HUGE_EXPLODE;
+			break;
+		case EXPLOSION_LARGE:
+			id = cn.nukkit.level.particle.Particle.TYPE_LARGE_EXPLODE;
+			break;
+		case EXPLOSION_NORMAL:
+			id = cn.nukkit.level.particle.Particle.TYPE_EXPLODE;
+			break;
+		case FALLING_DUST:
+			id = cn.nukkit.level.particle.Particle.TYPE_DUST;
+			break;
+		case FIREWORKS_SPARK:
+			break;
+		case FLAME:
+			id = cn.nukkit.level.particle.Particle.TYPE_FLAME;
+			break;
+		case FOOTSTEP:
+			break;
+		case HEART:
+			id = cn.nukkit.level.particle.Particle.TYPE_HEART;
+			break;
+		case ITEM_CRACK:
+			id = cn.nukkit.level.particle.Particle.TYPE_ITEM_BREAK;
+			break;
+		case ITEM_TAKE:
+			break;
+		case LAVA:
+			id = cn.nukkit.level.particle.Particle.TYPE_LAVA;
+			break;
+		case MOB_APPEARANCE:
+			break;
+		case NOTE:
+			break;
+		case PORTAL:
+			id = cn.nukkit.level.particle.Particle.TYPE_PORTAL;
+			break;
+		case REDSTONE:
+			id = cn.nukkit.level.particle.Particle.TYPE_REDSTONE;
+			break;
+		case SLIME:
+			id = cn.nukkit.level.particle.Particle.TYPE_SLIME;
+			break;
+		case SMOKE_LARGE:
+			id = cn.nukkit.level.particle.Particle.TYPE_LARGE_SMOKE;
+			break;
+		case SMOKE_NORMAL:
+			id = cn.nukkit.level.particle.Particle.TYPE_SMOKE;
+			break;
+		case SNOWBALL:
+			break;
+		case SNOW_SHOVEL:
+			id = cn.nukkit.level.particle.Particle.TYPE_SNOWBALL_POOF;
+			break;
+		case SPELL:
+			id = cn.nukkit.level.particle.Particle.TYPE_MOB_SPELL;
+			break;
+		case SPELL_INSTANT:
+			id = cn.nukkit.level.particle.Particle.TYPE_MOB_SPELL_INSTANTANEOUS;
+			break;
+		case SPELL_MOB:
+			id = cn.nukkit.level.particle.Particle.TYPE_MOB_SPELL;
+			break;
+		case SPELL_MOB_AMBIENT:
+			id = cn.nukkit.level.particle.Particle.TYPE_MOB_SPELL_AMBIENT;
+			break;
+		case SPELL_WITCH:
+			break;
+		case SUSPENDED:
+			break;
+		case SUSPENDED_DEPTH:
+			break;
+		case SWEEP_ATTACK:
+			break;
+		case TOWN_AURA:
+			id = cn.nukkit.level.particle.Particle.TYPE_TOWN_AURA;
+			break;
+		case VILLAGER_ANGRY:
+			id = cn.nukkit.level.particle.Particle.TYPE_VILLAGER_ANGRY;
+			break;
+		case VILLAGER_HAPPY:
+			id = cn.nukkit.level.particle.Particle.TYPE_VILLAGER_HAPPY;
+			break;
+		case WATER_BUBBLE:
+			break;
+		case WATER_DROP:
+			id = cn.nukkit.level.particle.Particle.TYPE_RAIN_SPLASH;
+			break;
+		case WATER_SPLASH:
+			id = cn.nukkit.level.particle.Particle.TYPE_WATER_SPLASH;
+			break;
+		case WATER_WAKE:
+			id = cn.nukkit.level.particle.Particle.TYPE_WATER_WAKE;
+			break;
+		default:
+			break;
+		
+		}
+		GenericParticle nukkitParticle = new GenericParticle(new Vector3(x, y, z), id);
+		
+		nukkit.addParticle(nukkitParticle);
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, double x, double y, double z, int count, double offsetX,
 			double offsetY, double offsetZ, T data) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, x, y, z, count, offsetX, offsetY, offsetZ, 0, data);
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, double x, double y, double z, int count, T data) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, x, y, z, count, 0, 0, 0, data);
 	}
 
 	@Override
 	public void spawnParticle(Particle particle, Location location, int count) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, 0, 0, 0);
 	}
 
 	@Override
 	public void spawnParticle(Particle particle, Location location, int count, double offsetX, double offsetY,
 			double offsetZ) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, offsetX, offsetY, offsetZ, 0);
 	}
 
 	@Override
 	public void spawnParticle(Particle particle, Location location, int count, double offsetX, double offsetY,
 			double offsetZ, double extra) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, offsetX, offsetY, offsetZ, extra, null);
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, Location location, int count, double offsetX, double offsetY,
 			double offsetZ, double extra, T data) {
-		throw Pokkit.unsupported();
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, offsetX, offsetY, offsetZ, extra, data);
 
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, Location location, int count, double offsetX, double offsetY,
 			double offsetZ, T data) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, offsetX, offsetY, offsetZ, 0, data);
 	}
 
 	@Override
 	public <T> void spawnParticle(Particle particle, Location location, int count, T data) {
-		throw Pokkit.unsupported();
-
+		spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, 0, 0, 0, 0, data);
 	}
 
 	@Override
