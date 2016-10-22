3303b9ad43540c17d671706bb64c4f64967e95c3
Langsdorf <firegamex001@gmail.com>
2016-10-22 18:59:42 -0200

    Some changes

--- src/main/java/nl/rutgerkok/pokkit/world/item/PokkitLeatherArmorMeta.java
+++ src/main/java/nl/rutgerkok/pokkit/world/item/PokkitLeatherArmorMeta.java
@@ -4,7 +4,6 @@ import org.bukkit.Color;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 import cn.nukkit.nbt.tag.CompoundTag;
-import nl.rutgerkok.pokkit.Pokkit;
 
 public class PokkitLeatherArmorMeta extends PokkitItemMeta implements LeatherArmorMeta {
 
@@ -12,16 +11,30 @@ public class PokkitLeatherArmorMeta extends PokkitItemMeta implements LeatherArm
 		super(tag);
 	}
 
+	
+	//If you find a better way to do this, PLEASE tell me.
 	@Override
 	public Color getColor() {
-		throw Pokkit.unsupported();
+		 if (!tag.exist("red")) return null;
+		 if (!tag.exist("green")) return null;
+		 if (!tag.exist("blue")) return null;
+		 int r = tag.getInt("red");
+		 int g = tag.getInt("green");
+		 int b = tag.getInt("blue");
+		 return Color.fromRGB(r, g, b);
 	}
-
+	
+	
 	@Override
 	public void setColor(Color color) {
-		//Not supported
+		int rgb = color.getRed() << 16 | color.getGreen() << 8 | color.getBlue();
+        this.getTag().putInt("customColor", rgb);//Set the color
+        this.getTag().putInt("red", color.getRed());//If you find a better way to do this, PLEASE tell me.
+        this.getTag().putInt("green", color.getGreen());//If you find a better way to do this, PLEASE tell me.
+        this.getTag().putInt("blue", color.getBlue());//If you find a better way to do this, PLEASE tell me.
 	}
-
+	
+	
 	@Override
 	public LeatherArmorMeta clone() {
 		return (LeatherArmorMeta) super.clone();
