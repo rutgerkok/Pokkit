93a3933aba1bfb78455b38bfefb1a36d412772e7
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-02-18 16:09:49 -0200

    Implement PokkitInventoryHolder

--- src/main/java/nl/rutgerkok/pokkit/inventory/PokkitInventoryHolder.java
+++ src/main/java/nl/rutgerkok/pokkit/inventory/PokkitInventoryHolder.java
@@ -1,13 +1,22 @@
 package nl.rutgerkok.pokkit.inventory;
 
-import nl.rutgerkok.pokkit.Pokkit;
-
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
-public class PokkitInventoryHolder {
-
+public class PokkitInventoryHolder implements InventoryHolder {
+    cn.nukkit.inventory.InventoryHolder nukkit;
+    
+    public PokkitInventoryHolder(cn.nukkit.inventory.InventoryHolder nukkit) {
+        this.nukkit = nukkit;
+    }
+    
 	public static InventoryHolder toBukkit(cn.nukkit.inventory.InventoryHolder holder) {
-		throw Pokkit.unsupported();
+		return new PokkitInventoryHolder(holder);
 	}
+
+    @Override
+    public Inventory getInventory() {
+        return PokkitInventory.toBukkit(nukkit.getInventory());
+    }
 
 }
