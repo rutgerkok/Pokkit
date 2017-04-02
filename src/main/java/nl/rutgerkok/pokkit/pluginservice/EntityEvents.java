ae5c03a6279af3cb16b0396644abff5c1757ce59
Rutger Kok <rutger.kok@live.com>
2017-04-02 11:55:56 +0200

    Fix up entity events
    * No longer using deprecated methods
    * Now checking the correct event when deciding whether to ignore an
    event. I added some documentation to EventTranslator to hopefully make
    things more clear. @MrPowerGamerBR
    * If a Bukkit plugin changes the damage, Nukkit is now informed of that
    * Fixed handling of subevents like EntityDamageByEntityEvent

--- src/main/java/nl/rutgerkok/pokkit/pluginservice/EntityEvents.java
+++ src/main/java/nl/rutgerkok/pokkit/pluginservice/EntityEvents.java
@@ -1,7 +1,17 @@
 package nl.rutgerkok.pokkit.pluginservice;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+
+import nl.rutgerkok.pokkit.PokkitLocation;
+import nl.rutgerkok.pokkit.entity.PokkitDamageCause;
+import nl.rutgerkok.pokkit.entity.PokkitEntity;
+import nl.rutgerkok.pokkit.entity.PokkitItemFrameEntity;
+import nl.rutgerkok.pokkit.entity.PokkitLivingEntity;
+import nl.rutgerkok.pokkit.world.PokkitBlock;
+import nl.rutgerkok.pokkit.world.item.PokkitItemStack;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.Creeper;
@@ -13,6 +23,7 @@ import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageModifier;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
@@ -23,130 +34,166 @@ import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.google.common.base.Function;
+
 import cn.nukkit.entity.EntityLiving;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.block.ItemFrameDropItemEvent;
 import cn.nukkit.item.Item;
-import nl.rutgerkok.pokkit.PokkitLocation;
-import nl.rutgerkok.pokkit.entity.PokkitDamageCause;
-import nl.rutgerkok.pokkit.entity.PokkitEntity;
-import nl.rutgerkok.pokkit.entity.PokkitItemFrameEntity;
-import nl.rutgerkok.pokkit.entity.PokkitLivingEntity;
-import nl.rutgerkok.pokkit.world.PokkitBlock;
-import nl.rutgerkok.pokkit.world.item.PokkitItemStack;
 
 public final class EntityEvents extends EventTranslator {
-    @EventHandler(ignoreCancelled = false)
-    public void onItemDrop(ItemFrameDropItemEvent event) {
-        // Bukkit uses EntityDamageByEntityEvent when someone hits an Item Frame
-        // However Nukkit uses ItemFrameDropItemEvent, so we are going to trigger an
-        // EntityDamageByEntityEvent
-        
-        // TODO: Do this the proper way... not the "let's use deprecated methods!" way
-        EntityDamageByEntityEvent bukkitEvent = new EntityDamageByEntityEvent(PokkitEntity.toBukkit(event.getPlayer()), new PokkitItemFrameEntity(event.getItemFrame()), EntityDamageEvent.DamageCause.ENTITY_ATTACK, 999D);
 
-        callCancellable(event, bukkitEvent);
-    }
-    
+	private static Map<DamageModifier, Double> getDamageMap(double baseDamage) {
+		Map<DamageModifier, Double> map = new EnumMap<>(DamageModifier.class);
+		map.put(DamageModifier.BASE, baseDamage);
+		return map;
+	}
+
+	private static Map<DamageModifier, Function<? super Double, Double>> getDamageModifierMap() {
+		Map<DamageModifier, Function<? super Double, Double>> map = new EnumMap<>(DamageModifier.class);
+		map.put(DamageModifier.BASE, val -> 0.0);
+		return map;
+	}
+
 	@EventHandler(ignoreCancelled = false)
 	public void onCreeperPower(cn.nukkit.event.entity.CreeperPowerEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+		if (canIgnore(CreeperPowerEvent.getHandlerList())) {
 			return;
 		}
 
 		PowerCause cause = PowerCause.valueOf(event.getCause().name()); // Dirty and fast conversion
-		
+
 		CreeperPowerEvent bukkitEvent = new CreeperPowerEvent((Creeper) PokkitEntity.toBukkit(event.getEntity()), cause);
 
 		callCancellable(event, bukkitEvent);
 	}
-	
+
 	@EventHandler(ignoreCancelled = false)
-	public void onBlockCombust(cn.nukkit.event.entity.EntityCombustByBlockEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+	public void onEntityCombust(cn.nukkit.event.entity.EntityCombustEvent event) {
+		if (canIgnore(EntityCombustEvent.getHandlerList())) {
 			return;
 		}
-		
+
+		// EntityCombustEvent and EntityCombustBy*Event share their handler
+		// lists. So this method can also be called with the event parameter set
+		// to an EntityCombustByEntityEvent or EntityCombustByBlockEvent. In that
+		// case, the event translation is a bit more involved.
+		if (event instanceof cn.nukkit.event.entity.EntityCombustByEntityEvent) {
+			onEntityCombustByEntity((cn.nukkit.event.entity.EntityCombustByEntityEvent) event);
+			return;
+		}
+		if (event instanceof cn.nukkit.event.entity.EntityCombustByBlockEvent) {
+			onEntityCombustByBlock((cn.nukkit.event.entity.EntityCombustByBlockEvent) event);
+			return;
+		}
+
+		EntityCombustEvent bukkitEvent = new EntityCombustEvent(PokkitEntity.toBukkit(event.getEntity()), event.getDuration());
+
+		callCancellable(event, bukkitEvent);
+		event.setDuration(bukkitEvent.getDuration());
+	}
+
+	public void onEntityCombustByBlock(cn.nukkit.event.entity.EntityCombustByBlockEvent event) {
+		// Called by onEntityCombust, so don't use an @EventHandler annotation
+
 		EntityCombustByBlockEvent bukkitEvent = new EntityCombustByBlockEvent(PokkitBlock.toBukkit(event.getCombuster()), PokkitEntity.toBukkit(event.getEntity()), event.getDuration());
 
 		callCancellable(event, bukkitEvent);
 	}
-	
-	@EventHandler(ignoreCancelled = false)
-	public void onEntityCombust(cn.nukkit.event.entity.EntityCombustByEntityEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
-			return;
-		}
-		
-		EntityCombustByEntityEvent bukkitEvent = new EntityCombustByEntityEvent(PokkitEntity.toBukkit(event.getCombuster()), PokkitEntity.toBukkit(event.getEntity()), event.getDuration());
+
+	private void onEntityCombustByEntity(cn.nukkit.event.entity.EntityCombustByEntityEvent event) {
+		// Called by onEntityCombust, so don't use an @EventHandler annotation
+
+		EntityCombustByEntityEvent bukkitEvent = new EntityCombustByEntityEvent(
+				PokkitEntity.toBukkit(event.getCombuster()),
+				PokkitEntity.toBukkit(event.getEntity()),
+				event.getDuration());
 
 		callCancellable(event, bukkitEvent);
+		event.setDuration(bukkitEvent.getDuration());
 	}
-	
-	@EventHandler(ignoreCancelled = false)
-	public void onCombust(cn.nukkit.event.entity.EntityCombustEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
-			return;
-		}
-		
-		EntityCombustEvent bukkitEvent = new EntityCombustEvent(PokkitEntity.toBukkit(event.getEntity()), event.getDuration());
 
-		callCancellable(event, bukkitEvent);
-	}
-	
-	@EventHandler(ignoreCancelled = false)
-	public void onBlockDamage(cn.nukkit.event.entity.EntityDamageByBlockEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
-			return;
-		}
-
-		// TODO: Do this the proper way... not the "let's use deprecated methods!" way
-		EntityDamageByBlockEvent bukkitEvent = new EntityDamageByBlockEvent(PokkitBlock.toBukkit(event.getDamager()), PokkitEntity.toBukkit(event.getEntity()), PokkitDamageCause.toBukkit(event.getCause()), event.getDamage());
-
-		callCancellable(event, bukkitEvent);
-	}
-	
-	@EventHandler(ignoreCancelled = false)
-	public void onEntityDamageByEntity(cn.nukkit.event.entity.EntityDamageByEntityEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
-			return;
-		}
-
-		// TODO: Do this the proper way... not the "let's use deprecated methods!" way
-		EntityDamageByEntityEvent bukkitEvent = new EntityDamageByEntityEvent(PokkitEntity.toBukkit(event.getDamager()), PokkitEntity.toBukkit(event.getEntity()), PokkitDamageCause.toBukkit(event.getCause()), event.getDamage());
-
-		callCancellable(event, bukkitEvent);
-	}
-	
 	@EventHandler(ignoreCancelled = false)
 	public void onEntityDamage(cn.nukkit.event.entity.EntityDamageEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+		if (canIgnore(EntityDamageEvent.getHandlerList())) {
 			return;
 		}
 
-		// TODO: Do this the proper way... not the "let's use deprecated methods!" way
-		EntityDamageEvent bukkitEvent = new EntityDamageEvent(PokkitEntity.toBukkit(event.getEntity()), PokkitDamageCause.toBukkit(event.getCause()), event.getCause());
+		// EntityDamageEvent and EntityDamageBy*Event share their handler
+		// lists. So this method can also be called with the event parameter set
+		// to an EntityDamageByEntityEvent or EntityDamageByBlockEvent. In that
+		// case, the event translation is a bit more involved.
+		if (event instanceof cn.nukkit.event.entity.EntityDamageByEntityEvent) {
+			onEntityDamageByEntity((cn.nukkit.event.entity.EntityDamageByEntityEvent) event);
+			return;
+		}
+		if (event instanceof cn.nukkit.event.entity.EntityDamageByBlockEvent) {
+			onEntityDamageByBlock((cn.nukkit.event.entity.EntityDamageByBlockEvent) event);
+			return;
+		}
+
+		// For now, only base damage is taken into account. Potions, armor etc
+		// are ignored
+		EntityDamageEvent bukkitEvent = new EntityDamageEvent(
+				PokkitEntity.toBukkit(event.getEntity()),
+				PokkitDamageCause.toBukkit(event.getCause()),
+				getDamageMap(event.getDamage()),
+				getDamageModifierMap());
 
 		callCancellable(event, bukkitEvent);
+		event.setDamage((float) bukkitEvent.getDamage());
 	}
-	
+
+	private void onEntityDamageByBlock(cn.nukkit.event.entity.EntityDamageByBlockEvent event) {
+		// Called by onEntityDamage, so don't use an @EventHandler annotation
+
+		// For now, only base damage is taken into account. Potions, armor etc
+		// are ignored
+		EntityDamageByBlockEvent bukkitEvent = new EntityDamageByBlockEvent(
+				PokkitBlock.toBukkit(event.getDamager()),
+				PokkitEntity.toBukkit(event.getEntity()),
+				PokkitDamageCause.toBukkit(event.getCause()),
+				getDamageMap(event.getDamage()),
+				getDamageModifierMap());
+
+		callCancellable(event, bukkitEvent);
+		event.setDamage((float) bukkitEvent.getDamage());
+	}
+
+	private void onEntityDamageByEntity(cn.nukkit.event.entity.EntityDamageByEntityEvent event) {
+		// Called by onEntityDamage, so don't use an @EventHandler annotation
+
+		// For now, only base damage is taken into account. Potions, armor etc
+		// are ignored
+		EntityDamageByEntityEvent bukkitEvent = new EntityDamageByEntityEvent(
+				PokkitEntity.toBukkit(event.getDamager()),
+				PokkitEntity.toBukkit(event.getEntity()),
+				PokkitDamageCause.toBukkit(event.getCause()),
+				getDamageMap(event.getDamage()),
+				getDamageModifierMap());
+
+		callCancellable(event, bukkitEvent);
+		event.setDamage((float) bukkitEvent.getDamage());
+	}
+
 	@EventHandler(ignoreCancelled = false)
 	public void onEntityDeath(cn.nukkit.event.entity.EntityDeathEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+		if (canIgnore(EntityDeathEvent.getHandlerList())) {
 			return;
 		}
 
-		List<ItemStack> drops = new ArrayList<ItemStack>();
-		
+		List<ItemStack> bukkitDrops = new ArrayList<ItemStack>();
 		for (Item item : event.getDrops()) {
-			drops.add(PokkitItemStack.toBukkitCopy(item));
+			bukkitDrops.add(PokkitItemStack.toBukkitCopy(item));
 		}
 
-		EntityDeathEvent bukkitEvent = new EntityDeathEvent(PokkitLivingEntity.toBukkit((EntityLiving) event.getEntity()), drops);
+		EntityDeathEvent bukkitEvent = new EntityDeathEvent(
+				PokkitLivingEntity.toBukkit((EntityLiving) event.getEntity()), bukkitDrops);
 
 		callUncancellable(bukkitEvent);
+		event.setDrops(bukkitEvent.getDrops().stream().map(PokkitItemStack::toNukkitCopy).toArray(Item[]::new));
 	}
-	
+
 	@EventHandler(ignoreCancelled = false)
 	public void onEntityExplode(cn.nukkit.event.entity.EntityExplodeEvent event) {
 		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
@@ -154,25 +201,25 @@ public final class EntityEvents extends EventTranslator {
 		}
 
 		List<Block> blocks = new ArrayList<Block>();
-		
+
 		for (cn.nukkit.block.Block nukkitBlock : event.getBlockList()) {
 			blocks.add(PokkitBlock.toBukkit(nukkitBlock));
 		}
-		
+
 		EntityExplodeEvent bukkitEvent = new EntityExplodeEvent(PokkitEntity.toBukkit(event.getEntity()), PokkitLocation.toBukkit(event.getPosition()), blocks, (float) event.getYield());
 
 		callCancellable(event, bukkitEvent);
 	}
-	
+
 	@EventHandler(ignoreCancelled = false)
 	public void onEntityRegainHealth(cn.nukkit.event.entity.EntityRegainHealthEvent event) {
 		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
 			return;
 		}
-		
+
 		int reason = event.getRegainReason();
 		RegainReason bukkitReason = RegainReason.EATING;
-		
+
 		switch (reason) {
 		case cn.nukkit.event.entity.EntityRegainHealthEvent.CAUSE_MAGIC:
 			bukkitReason = RegainReason.MAGIC;
@@ -187,40 +234,60 @@ public final class EntityEvents extends EventTranslator {
 			bukkitReason = RegainReason.REGEN;
 			break;
 		}
-		
-		EntityRegainHealthEvent bukkitEvent = new EntityRegainHealthEvent(PokkitEntity.toBukkit(event.getEntity()), (double) event.getAmount(), bukkitReason);
+
+		EntityRegainHealthEvent bukkitEvent = new EntityRegainHealthEvent(PokkitEntity.toBukkit(event.getEntity()), event.getAmount(), bukkitReason);
 
 		callCancellable(event, bukkitEvent);
 	}
-	
+
+	@EventHandler(ignoreCancelled = false)
+	public void onExplosion(cn.nukkit.event.entity.ExplosionPrimeEvent event) {
+		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+			return;
+		}
+
+		ExplosionPrimeEvent bukkitEvent = new ExplosionPrimeEvent(PokkitEntity.toBukkit(event.getEntity()), (float) event.getForce(), true); // Always true because Nukkit doesn't have that boolean
+
+		callCancellable(event, bukkitEvent);
+	}
+
+	@EventHandler(ignoreCancelled = false)
+    public void onItemDrop(ItemFrameDropItemEvent event) {
+        // Bukkit uses EntityDamageByEntityEvent when someone hits an Item Frame
+        // However Nukkit uses ItemFrameDropItemEvent, so we are going to trigger an
+        // EntityDamageByEntityEvent
+
+		if (canIgnore(EntityDamageByEntityEvent.getHandlerList())) {
+			return;
+		}
+
+        EntityDamageByEntityEvent bukkitEvent = new EntityDamageByEntityEvent(
+        		PokkitEntity.toBukkit(event.getPlayer()),
+        		new PokkitItemFrameEntity(event.getItemFrame()),
+        		EntityDamageEvent.DamageCause.ENTITY_ATTACK,
+        		getDamageMap(999D),
+        		getDamageModifierMap());
+
+        callCancellable(event, bukkitEvent);
+    }
+
 	@EventHandler(ignoreCancelled = false)
 	public void onSpawn(cn.nukkit.event.entity.EntitySpawnEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+		if (canIgnore(EntitySpawnEvent.getHandlerList())) {
 			return;
 		}
 		EntitySpawnEvent bukkitEvent = new EntitySpawnEvent(PokkitEntity.toBukkit(event.getEntity()));
 
 		callUncancellable(bukkitEvent);
 	}
-	
+
 	@EventHandler(ignoreCancelled = false)
 	public void onTeleport(cn.nukkit.event.entity.EntityTeleportEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
+		if (canIgnore(EntityTeleportEvent.getHandlerList())) {
 			return;
 		}
-		
-		EntityTeleportEvent bukkitEvent = new EntityTeleportEvent(PokkitEntity.toBukkit(event.getEntity()), PokkitLocation.toBukkit(event.getFrom()), PokkitLocation.toBukkit(event.getTo()));
 
-		callCancellable(event, bukkitEvent);
-	}
-	
-	@EventHandler(ignoreCancelled = false)
-	public void onExplosion(cn.nukkit.event.entity.ExplosionPrimeEvent event) {
-		if (canIgnore(PlayerInteractEvent.getHandlerList())) {
-			return;
-		}
-		
-		ExplosionPrimeEvent bukkitEvent = new ExplosionPrimeEvent(PokkitEntity.toBukkit(event.getEntity()), (float) event.getForce(), true); // Always true because Nukkit doesn't have that boolean
+		EntityTeleportEvent bukkitEvent = new EntityTeleportEvent(PokkitEntity.toBukkit(event.getEntity()), PokkitLocation.toBukkit(event.getFrom()), PokkitLocation.toBukkit(event.getTo()));
 
 		callCancellable(event, bukkitEvent);
 	}
--- src/main/java/nl/rutgerkok/pokkit/pluginservice/EventTranslator.java
+++ src/main/java/nl/rutgerkok/pokkit/pluginservice/EventTranslator.java
@@ -14,16 +14,64 @@ import cn.nukkit.plugin.PluginBase;
  */
 abstract class EventTranslator implements PokkitService, Listener {
 
+	/**
+	 * Fires the event using {@code Bukkit.getPluginManager().callEvent(event)}.
+	 * Propagates whether the event is cancelled before and after calling the
+	 * event.
+	 *
+	 * @param nukkit
+	 *            The original Nukkit event.
+	 * @param bukkit
+	 *            The translated Bukkit event. This argument must extend
+	 *            {@link Event} <i>and</i> implement {@link Cancellable}.
+	 */
 	protected void callCancellable(cn.nukkit.event.Cancellable nukkit, Cancellable bukkit) {
 		bukkit.setCancelled(nukkit.isCancelled());
 		Bukkit.getPluginManager().callEvent((Event) bukkit);
 		nukkit.setCancelled(bukkit.isCancelled());
 	}
 
+	/**
+	 * Shortcut for {@code Bukkit.getPluginManager().callEvent(event)}
+	 *
+	 * @param bukkit
+	 *            The Bukkit event.
+	 */
 	protected void callUncancellable(Event bukkit) {
 		Bukkit.getPluginManager().callEvent(bukkit);
 	}
 
+	/**
+	 * This method checks if a handler list is empty. The handler list is the
+	 * list of all Bukkit plugins that are "handling" (responding to) an event.
+	 * If the list is emtpy, then no plugins are listening to that event, and it
+	 * can be safely ignored.
+	 *
+	 * <p>
+	 * This method is intended to be used as follows:
+	 *
+	 * <pre>
+	 * &commat;EventHandler(ignoreCancelled = false)
+	 * public void onSpawn(cn.nukkit.event.entity.EntitySpawnEvent event) {
+	 *     if (canIgnore(EntitySpawnEvent.getHandlerList())) {
+	 *         // No Bukkit events are listening to EntitySpawnEvent, so
+	 *         // don't waste time firing it.
+	 *         return;
+	 *     }
+	 *
+	 *     // Spend time to convert and fire the event, so that Bukkit
+	 *     // plugins can react
+	 *     EntitySpawnEvent bukkitEvent = ...
+	 * }
+	 * </pre>
+	 *
+	 * In the above example, we ignore {@code EntitySpawnEvent} if
+	 * {@code EntitySpawnEvent} has no handlers.
+	 *
+	 * @param handlerList
+	 *            The plugins listening to a Bukkit event.
+	 * @return True if the handler list is empty, false otherwise.
+	 */
 	protected boolean canIgnore(HandlerList handlerList) {
 		return handlerList.getRegisteredListeners().length == 0;
 	}
